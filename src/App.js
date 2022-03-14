5d1b310ce6a430eac1b9c0ff9680ba6e7b3e3e39
idpan <idpanashari813@gmail.com>
2022-03-14 20:53:58 +0700

    menyelesaikan navbar.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import "./App.css";
 import Navbar from "./component/Navbar";
 import Home from "./component/Home";
 import About from "./component/About";
+import { useState } from "react";
 function App() {
+  const [home, setHome] = useState({
+    headingText: "Home Shop About Contact",
+    paragraph:
+      "Discover innovative ways to decorate  We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+    image: "/images/desktop-image-hero-1.jpg",
+  });
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Home></Home>
+      <Home headingText={home.headingText} paragraph={home.paragraph} image={home.image}></Home>
       <About></About>
     </div>
   );
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,20 +1,46 @@
 export default function Navbar(props) {
   return (
-    <nav>
-      <div className="hamburger-menu">
+    <nav className="flex sm:justify-start items-center gap-10  fixed justify-between right-0 left-0 pt-10 px-7">
+      <input className="peer hidden" type="checkbox" id="navbar" />
+      <label htmlFor="navbar" className="sm:hidden cursor-pointer">
         <img src="/images/icon-hamburger.svg" alt="" />
-      </div>
-      <div>
+      </label>
+      <div className="ml-10">
         <a href="#home">
-          <img src="/images/logo.svg" alt="hero" />
+          <img src="/images/logo.svg" alt="logo" />
         </a>
       </div>
-      <ul>
-        <li>home</li>
-        <li>shop</li>
-        <li>about</li>
-        <li>contact</li>
-      </ul>
+
+      <div className="sm:hidden "></div>
+
+      <div className="flex h-0 sm:h-fit sm:opacity-100 sm:static sm:bg-transparent peer-checked:h-32 peer-checked:opacity-100  transition-opacity ease-in duration-300  opacity-0   items-center justify-between fixed left-0 top-0 right-0 px-8 bg-white ">
+        <label htmlFor="navbar" className="sm:hidden cursor-pointer">
+          <img src="/images/icon-close.svg" alt="close-icon" className="pointer" />
+        </label>
+
+        <ul className="flex gap-14 right-9 font-bold">
+          <li>
+            <a className="text-white" href="home">
+              home
+            </a>
+          </li>
+          <li>
+            <a className="text-white" href="shop">
+              shop
+            </a>
+          </li>
+          <li>
+            <a className="text-white" href="about">
+              about
+            </a>
+          </li>
+          <li>
+            <a className="text-white" href="contact">
+              contact
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
